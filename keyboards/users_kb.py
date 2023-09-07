2ce823247416d95806f8317890d5469454f59960
MorevDA <morevda1983@gmail.com>
2023-09-08 00:01:55 +0300

    В словарь с перечнем филиалов добавлены филиалы расположенные в Левобережном районе.

--- keyboards/users_kb.py
+++ keyboards/users_kb.py
@@ -4,6 +4,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 # Функция, генерирующая клавиатуру из словаря(для выбора района, филиала, способа сортировки)
 def create_inline_keyboard_dict(data:dict) -> InlineKeyboardBuilder:
+    """Функция для генерации инлайн клавиатуры из словаря."""
     # Инициализируем билдер
     keyboard: InlineKeyboardBuilder = InlineKeyboardBuilder()
     # Добавляем в билдер кнопки
@@ -13,8 +14,17 @@ def create_inline_keyboard_dict(data:dict) -> InlineKeyboardBuilder:
     return keyboard.as_markup(resize_keyboard=True)
 
 
-# Функция, генерирующая клавиатуру при множественном выборе
+def create_inline_keyboad_in_list(data:list) -> InlineKeyboardBuilder:
+    """Функция для генерации инлайн клавиатуры из списка"""
+    keyboard: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    for elem in data:
+        keyboard.row(InlineKeyboardButton(text=elem, callback_data=elem))
+    return keyboard.as_markup(resize_keyboard=True)
+
+
+
 def create_inline_keyboard_multiple_choice(data: dict) -> InlineKeyboardBuilder:
+    """Функция для генерации инлайн клавиатуры для множественного выбора"""
     # Инициализируем билдер
     keyboard: InlineKeyboardBuilder = InlineKeyboardBuilder()
     # Добавляем в билдер кнопки
